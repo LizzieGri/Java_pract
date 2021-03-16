fb9372678f42a15ad03d71a9a608bd9630f77ec6
LizzieGri <65723977+LizzieGri@users.noreply.github.com>
2021-03-16 13:45:02 +0300

    done Pract_7

--- Pract_7/PR7_2.java
+++ Pract_7/PR7_2.java
@@ -1,4 +1,39 @@
 package Homework.Java_PW2.Pract_7;
 
+interface Image {
+    void display();
+}
+class RealImage implements Image {
+    private String file;
+    public RealImage(String file){
+        this.file = file;
+        load(file);
+    }
+    private void load(String file){
+        System.out.println("Загрузка " + file);
+    }
+    public void display() {
+        System.out.println("Просмотр " + file);
+    }
+}
+class ProxyImage implements Image {
+    private String file;
+    private RealImage image;
+    public ProxyImage(String file){
+        this.file = file;
+    }
+    public void display() {
+        if(image == null){
+            image = new RealImage(file);
+        }
+        image.display();
+    }
+}
+
 public class PR7_2 {
+    public static void main(String[] args) {
+        Image image = new ProxyImage("test.jpg");
+        image.display();
+        image.display();
+    }
 }
