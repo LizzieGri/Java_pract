9bc3622119211df34b5d6432366fa59ae57ee824
LizzieGri <65723977+LizzieGri@users.noreply.github.com>
2021-05-30 19:45:14 +0300

    Pract_15

---
+++ Pract_15/CardController.java
@@ -0,0 +1,28 @@
+package Pract_15;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+public class CardController {
+    @Autowired
+    private NewCard newCard;
+
+    @PostMapping("/card")
+    public void post(@RequestBody Card card) {
+        newCard.addCard(card);
+    }
+
+    @GetMapping("/cards")
+    public List<Card> getAll() {
+        return newCard.getCards();
+    }
+
+    @DeleteMapping("/card/{id}")
+    public void delete(@PathVariable UUID id) {
+        newCard.deleteManufacture(id);
+    }
+}
---
+++ Pract_15/Main_fifteen.java
@@ -0,0 +1,11 @@
+package Pract_15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main_fifteen {
+    public static void main(String[] args) {
+        SpringApplication.run(Main_fifteen.class, args);
+    }
+}
